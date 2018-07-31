cdd37f5dc38e8aba77a79b874124894ed8ab2f25
ekristina <panacejja@gmail.com>
2018-07-31 16:27:47 -0400

    cookie support, refactored

--- headlines.py
+++ headlines.py
@@ -1,10 +1,12 @@
 from urllib.parse import quote
 
+import datetime
 import feedparser
 import json
 import requests
 
 from flask import Flask
+from flask import make_response  # create a response object to set cookies on
 from flask import render_template
 from flask import request
 
@@ -68,46 +70,55 @@ def get_news(source):
     return feed['entries']
 
 
+def get_value_with_fallback(key):
+
+    if request.args.get(key):
+        return request.args.get(key)
+
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+
+    return DEFAULTS[key]
+
+
 @app.route("/")
 def home():
 
     # get rss
 
-    source = request.args.get("source")
-    if not source or source.lower() not in RSS_FEEDS:
-        source = DEFAULTS['source']
-
-    else:
-        source = source.lower()
+    source = get_value_with_fallback("source")
 
     articles = get_news(source)
 
     # get weather
 
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback("city")
 
     weather = get_weather(city)
 
     # get customized currency
 
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
 
     rate, currencies = get_rate(currency_from, currency_to)
 
-    return render_template("home.html",
-                           articles=articles,
-                           weather=weather,
-                           currency_from=currency_from,
-                           currency_to=currency_to,
-                           rate=rate,
-                           currencies=sorted(currencies))
+    # save cookies and return template
+
+    response = make_response(render_template("home.html",
+                                             articles=articles,
+                                             weather=weather,
+                                             currency_from=currency_from,
+                                             currency_to=currency_to,
+                                             rate=rate,
+                                             currencies=sorted(currencies)))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("source", source, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from",
+                        currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
 
 
 if __name__ == '__main__':
